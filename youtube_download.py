6d2e8cbceae574655da9276d8718c9a9059f0510
Aki-R <akiakr.s@gmail.com>
2024-04-23 19:36:25 +0900

    use argparse for arguments

--- youtube_download.py
+++ youtube_download.py
@@ -1,26 +1,29 @@
+import argparse
 from pytube import YouTube
-import ffmpeg
 from pathlib import Path
 from spleeter.separator import Separator
+import ffmpeg
 
-PATH_OUTPUT = "./movie"
-URL_YOUTUBE = "https://www.youtube.com/watch?v=da26TN6N7E0"
 
-if __name__ == "__main__":
-    yt = YouTube(URL_YOUTUBE)
-    yt.streams.filter(file_extension='mp4').get_highest_resolution().download(output_path=PATH_OUTPUT,
-                                                                              filename="out.mp4")
+def download_and_separate(url, output_dir):
+    yt = YouTube(url)
+    yt.streams.filter(file_extension='mp4').get_highest_resolution().download(output_path=output_dir, filename="out.mp4")
 
-    path_out_dir = Path(PATH_OUTPUT)
+    path_out_dir = Path(output_dir)
     path_mp4 = path_out_dir / "out.mp4"
     path_mp3 = path_out_dir / "out.mp3"
 
-    stream = ffmpeg.input(path_mp4)
-    stream = ffmpeg.output(stream, str(path_mp3))
-    try:
-        ffmpeg.run(stream)
-    except Exception as e:
-        print(e)
+    ffmpeg.input(str(path_mp4)).output(str(path_mp3)).run()
 
     separator = Separator('spleeter:2stems')
     separator.separate_to_file(str(path_mp3), str(path_out_dir), duration=1000)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='YouTubeの動画をダウンロードし、音声を分離します。')
+    parser.add_argument('--url', '-i', type=str, help='YouTubeの動画URL')
+    parser.add_argument('--output-dir', '-o', type=str, default='.', help='出力先ディレクトリ')
+
+    args = parser.parse_args()
+
+    download_and_separate(args.url, args.output_dir)
